69bbf2cbd0bfb59a2b606e9e99062166cb348015
NolaAde <147328045+NolaAde@users.noreply.github.com>
2023-10-22 16:00:06 +0000

    first commit

---
+++ project.py
@@ -0,0 +1,68 @@
+# print(5+5)
+# print(2+2)
+# print(4+4)
+# print(8+8)
+# print(16+16)
+# print(32+32)
+# print(248-1)
+# print(248-10)
+# print(248-100)
+# print(248-2)
+# print(248-20)
+# print(248-200)
+# print(248*2)
+# print(248*3)
+# print(248*4)
+# print(248*5)
+# print(248*6)
+# print(248/1)
+# print(248/3)
+# print(248/5)
+name = "Nola"
+print(name)
+
+age = 9
+print(age)
+
+i_like_ice_cream = True
+print(i_like_ice_cream)
+
+favourite_movie = "movie_title_OVER_THE_MOON"
+print(favourite_movie)
+
+favourite_colour = "all_colours"
+print(favourite_colour)
+
+favourite_subject = "maths"
+print(favourite_subject)
+
+i_like_orange = True
+print(i_like_orange)
+
+i_like_bananas = False
+print(i_like_bananas)
+
+birth_month = "June"
+print(birth_month)
+
+height = 4.2
+print(height)
+
+
+
+
+
+best_friend = "Laila Zaid"
+print(best_friend)
+
+favourite_sport = "gymnastics"
+print(favourite_sport)
+
+favourite_food = "pasta"
+print(favourite_food)
+
+favourite_book = "rainbow magic"
+print(favourite_book)
+
+favourite = "black panther"
+print(name)
