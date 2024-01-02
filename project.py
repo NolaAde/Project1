68312c9ea3679d176085cacf8e4b63c35ca2eb33
NolaAde <147328045+NolaAde@users.noreply.github.com>
2024-01-02 13:21:43 +0000

    merry christmas and a happy new year

--- project.py
+++ project.py
@@ -1,23 +1,23 @@
-# print(5+5)
-# print(2+2)
-# print(4+4)
-# print(8+8)
-# print(16+16)
-# print(32+32)
-# print(248-1)
-# print(248-10)
-# print(248-100)
-# print(248-2)
-# print(248-20)
-# print(248-200)
-# print(248*2)
-# print(248*3)
-# print(248*4)
-# print(248*5)
-# print(248*6)
-# print(248/1)
-# print(248/3)
-# print(248/5)
+print(5+5)
+print(2+2)
+print(4+4)
+print(8+8)
+print(16+16)
+print(32+32)
+print(248-1)
+print(248-10)
+print(248-100)
+print(248-2)
+print(248-20)
+print(248-200)
+print(248*2)
+print(248*3)
+print(248*4)
+print(248*5)
+print(248*6)
+print(248/1)
+print(248/3)
+print(248/5)
 name = "Nola"
 print(name)
 
@@ -39,8 +39,8 @@ print(favourite_subject)
 i_like_orange = True
 print(i_like_orange)
 
-i_like_bananas = False
-print(i_like_bananas)
+i_hate_bananas = False
+print(i_hate_bananas)
 
 birth_month = "June"
 print(birth_month)
@@ -61,7 +61,7 @@ print(favourite_sport)
 favourite_food = "pasta"
 print(favourite_food)
 
-favourite_book = "rainbow magic"
+favourite_book = "dork diaries"
 print(favourite_book)
 
 favourite_character = "black panther"
